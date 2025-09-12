75d19dc8c39db4e7294f19689e17a53cdffb5cde
ana.su._ <98901363+ANASUDDEEN-PP@users.noreply.github.com>
2025-09-12 15:12:40 +0530

    setup online payment integration

--- Routes/orderRoute.js
+++ Routes/orderRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const orderController = require("../Controllers/orderController")
+const orderController = require("../Controllers/orderController");
+const razorPayController = require("../Controllers/onlinePaymentController")
 
 router.post('/add', orderController.addOrder);
 router.post('/gpay/payment/details', orderController.googlePayPaymentDetails);
@@ -8,8 +9,12 @@ router.get('/get/all/orders', orderController.getAllOrders);
 router.get('/get/order/:id', orderController.getOrderById);
 router.put('/edit/:id', orderController.orderEditByAdmin);
 router.get('/user/get/:id', orderController.orderDetailsById);
-router.put('/user/cancel/:id', orderController.userCancelOrder)
+router.put('/user/cancel/:id', orderController.userCancelOrder);
+
+//Razorpay Integration
+router.post("/razorpay/create-order", razorPayController.createRazorpayOrder);
+router.post("/razorpay/verify-payment", razorPayController.verifyRazorpayPayment);
 
 module.exports = router;
 
-// 
+// /order/
